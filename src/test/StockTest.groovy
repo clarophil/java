b682f359d15bb5ec141c502386c8c6deae81d4f6
clarophil <philippe.dekimpe@gmail.com>
2017-12-01 12:12:01 +0100

    add test

---
+++ src/test/StockTest.groovy
@@ -0,0 +1,10 @@
+class StockTest extends groovy.util.GroovyTestCase {
+    void testGetMostExpensive() {
+    }
+
+    void testMoyenne() {
+    }
+
+    void testEcarttype() {
+    }
+}
---
+++ src/test/VoitureTest.groovy
@@ -0,0 +1,7 @@
+class VoitureTest extends groovy.util.GroovyTestCase {
+    void testGetPrice() {
+    }
+
+    void testGetModele() {
+    }
+}
